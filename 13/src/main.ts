d09e9d70dfd4c1014affd4c11479e08f971f72f7
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-12-07 10:01:53 +0100

    lesson 13 - add sky

--- 13/src/main.ts
+++ 13/src/main.ts
@@ -1,6 +1,6 @@
 import "./styles.css";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { OrbitControls, Sky } from "three/examples/jsm/Addons.js";
 import { Timer } from "three/addons/misc/Timer.js";
 import GUI from "lil-gui";
 
@@ -442,6 +442,21 @@ ghost2.shadow.camera.far = 10;
 ghost3.shadow.mapSize.width = 256;
 ghost3.shadow.mapSize.height = 256;
 ghost3.shadow.camera.far = 10;
+
+/**
+ * Sky
+ */
+
+const sky = new Sky();
+// sky.scale.setScalar(100);
+sky.scale.set(100, 100, 100);
+scene.add(sky);
+
+sky.material.uniforms["turbidity"].value = 10;
+sky.material.uniforms["rayleigh"].value = 3;
+sky.material.uniforms["mieCoefficient"].value = 0.1;
+sky.material.uniforms["mieDirectionalG"].value = 0.95;
+sky.material.uniforms["sunPosition"].value.set(0.3, -0.038, -0.95);
 
 /**
  * Animate
