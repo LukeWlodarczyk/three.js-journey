323bc745e9f55f61c5bcb7cd249c358f4cf5e9e4
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-12-10 13:35:15 +0100

    lesson 17 - apply forces

--- 17/src/main.ts
+++ 17/src/main.ts
@@ -69,6 +69,11 @@ const sphereBody = new CANNON.Body({
   shape: sphereShape,
 });
 
+sphereBody.applyLocalForce(
+  new CANNON.Vec3(150, 0, 0),
+  new CANNON.Vec3(0, 0, 0)
+);
+
 world.addBody(sphereBody);
 
 // Floor
@@ -195,6 +200,8 @@ const tick = () => {
   oldElapsedTime = elapsedTime;
 
   // Update physics world
+  sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position);
+
   world.step(1 / 60, deltaTime, 3);
 
   // sphere.position.x = sphereBody.position.x;
