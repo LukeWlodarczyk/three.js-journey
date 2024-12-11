076682408ad73d3f94d947275559b84a73d35c22
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-12-11 16:13:20 +0100

    lesson 19 - cast a ray through spheres

--- 19/src/main.ts
+++ 19/src/main.ts
@@ -38,6 +38,12 @@ object3.position.x = 2;
 scene.add(object1, object2, object3);
 
 /**
+ * Raycaster
+ */
+
+const raycaster = new THREE.Raycaster();
+
+/**
  * Sizes
  */
 const sizes = {
@@ -92,6 +98,34 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Animate objects
+
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+  // Cast a ray
+  const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  const rayDirection = new THREE.Vector3(1, 0, 0);
+  rayDirection.normalize();
+
+  raycaster.set(rayOrigin, rayDirection);
+
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
+
+  console.log(intersects);
+
+  objectsToTest.forEach((object) => {
+    object.material.color.set("red");
+  });
+
+  intersects.forEach((intersect) => {
+    (
+      (intersect.object as THREE.Mesh).material as THREE.MeshBasicMaterial
+    ).color.set("blue");
+  });
 
   // Update controls
   controls.update();
