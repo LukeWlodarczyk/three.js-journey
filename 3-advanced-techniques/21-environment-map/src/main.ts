7bda994914a2368ebdd843f8354b487250236d36
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-12-16 20:26:33 +0100

    lesson 21 - add hdr env map

--- 3-advanced-techniques/21-environment-map/src/main.ts
+++ 3-advanced-techniques/21-environment-map/src/main.ts
@@ -2,7 +2,7 @@ import "./styles.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
-import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { GLTFLoader, RGBELoader } from "three/examples/jsm/Addons.js";
 
 /**
  * Loaders
@@ -10,6 +10,8 @@ import { GLTFLoader } from "three/examples/jsm/Addons.js";
 const gltfLoader = new GLTFLoader();
 
 const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+const rgbeLoader = new RGBELoader();
 
 /**
  * Base
@@ -51,18 +53,28 @@ gui
   .name("environmentRotationY");
 
 // LDR cube texture
+// const environmentMap = cubeTextureLoader.load([
+//   "./environmentMaps/0/px.png",
+//   "./environmentMaps/0/nx.png",
+//   "./environmentMaps/0/py.png",
+//   "./environmentMaps/0/ny.png",
+//   "./environmentMaps/0/pz.png",
+//   "./environmentMaps/0/nz.png",
+// ]);
 
-const environmentMap = cubeTextureLoader.load([
-  "./environmentMaps/0/px.png",
-  "./environmentMaps/0/nx.png",
-  "./environmentMaps/0/py.png",
-  "./environmentMaps/0/ny.png",
-  "./environmentMaps/0/pz.png",
-  "./environmentMaps/0/nz.png",
-]);
+// scene.environment = environmentMap;
+// scene.background = environmentMap;
 
-scene.environment = environmentMap;
-scene.background = environmentMap;
+// HDR (RGBE) equirectangular
+
+rgbeLoader.load("./environmentMaps/0/2k.hdr", (environmentMap) => {
+  console.log(environmentMap);
+
+  environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+
+  scene.background = environmentMap;
+  scene.environment = environmentMap;
+});
 
 /**
  * Torus Knot
